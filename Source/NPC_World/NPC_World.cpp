20e54e0c93491528375c16ccd6dcbb28b44975f9
JeongHun <45549589+ojh6507@users.noreply.github.com>
2023-08-28 02:23:21 +0900

    Revert "init"
    This reverts commit bac8a1291664141597caf803566838ff93200d04.

--- Source/NPC_World/NPC_World.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "NPC_World.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NPC_World, "NPC_World" );
- 
